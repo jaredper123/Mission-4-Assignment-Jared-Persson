ef6ec5d5af962af03f374092eeb8f06f2a925f57
jaredper123 <jaredper@byu.edu>
2023-02-06 21:34:32 -0700

    Mission 5 changes

---
+++ WebApplication1/Controllers/BlahController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    public class BlahController : Controller
+    {
+        public IActionResult Index() //loading main page
+        {
+            return View();
+        }
+
+        [HttpGet] //loading page without submission
+        public IActionResult GradeCalc ()
+        {
+            return View();
+        }
+
+        [HttpPost] //loading page with submission
+        public IActionResult GradeCalc (GradeCalcModel model)
+        {
+            return View();
+        }
+    }
+}
